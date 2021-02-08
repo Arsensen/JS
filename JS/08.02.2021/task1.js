c40d704260738865452523f79c3dde160b40a955
SirArt <siraziarty@gmail.com>
2021-02-08 14:11:09 +0300

    Задачи по Прототипам

---
+++ JS/08.02.2021/task1.js
@@ -0,0 +1,25 @@
+/* Реализовать Person class который имеет
+name: string
+getName: () => string */
+
+function Person(str){
+    this.name = str;
+    this.getName = ()=>this.name;
+}
+
+
+
+/* Реализовать Man class который наследуется от Person
+name: string
+faialHair: boolean
+getName: () => string
+getFacialHair: () => boolean */
+
+function Man(str, bool){
+    Person.call(this, str);
+    this.facialHair = bool;
+    this.getFacialHair = ()=>this.facialHair;
+}
+
+Man.prototype = Object.create(Person.prototype);
+Man.prototype.constructor = Man;
